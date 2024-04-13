3cc04002aee452b9063006cf7944ad0fec434cce
robweekes <weekes.robert@gmail.com>
2024-04-13 15:26:34 -0700

    problem 11

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -7,15 +7,60 @@ Do not use the built in Array.flat.
 
 Examples:
 
-flatten([]); // []
-flatten([1, 2]); // [1, 2]
-flatten([1, [2, [3]]]); // [1, 2, 3]
+flatten2([]); // []
+flatten2([1, 2]); // [1, 2]
+flatten2([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
+// let newArr = [];
+
+// function flatten2(arr) {
+//     if(arr.length === 0) {
+//         return arr;
+//     } else {
+//         console.log(arr);
+//         let last = arr[arr.length-1];
+//         console.log(last);
+
+//         arr.pop(last);
+//         console.log(arr);
+//         newArr.push(last);
+//         console.log(newArr);
+
+//         return flatten2(arr);
+//     }
+
+// }
+
+// console.log(flatten2([])); // []
+// console.log(flatten2([1, 2])); // [1, 2]
+// console.log(flatten2([1,
+//                       [2,
+//                         [3]]])); //
+
+
 function flatten(arr) {
-  // Your code here 
+    if(arr.length === 0) {
+        return [];
+    }
+
+    // if first element is an array, recursively flatten it
+    if(Array.isArray(arr[0])) {
+                                              // slices everything off of the first index
+        return flatten(arr[0]).concat(flatten(arr.slice(1)));
+    }
+    // if element is not an array, put it in array and concat with flatten remainder of array
+    return [arr[0]].concat(flatten(arr.slice(1)));
 }
-  
+
+
+// console.log(flatten([])); // []
+// console.log(flatten([1, 2])); // [1, 2]
+// console.log(flatten([1, 2, 3, 4, 5])); // [1, 2, 3, 4, 5]
+console.log(flatten([1,
+                      [2,
+                        [3]]])); // [1, 2, 3]
+
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
-  
