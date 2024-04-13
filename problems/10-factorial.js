2531e72bd2cdc24189ceec30462a2d15cc8d8dc8
robweekes <weekes.robert@gmail.com>
2024-04-13 14:22:47 -0700

    console logs 10

--- problems/10-factorial.js
+++ problems/10-factorial.js
@@ -26,9 +26,9 @@ function factorial(num) {
   }
 }
 
-factorial(1); // 1
-factorial(3); // 6
-factorial(5); // 120
+console.log(factorial(1)); // 1
+console.log(factorial(3)); // 6
+console.log(factorial(5)); // 120
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = factorial;
