89fd758654240b53d232a48c837897293ae656f8
robweekes <weekes.robert@gmail.com>
2024-04-13 16:41:01 -0700

    updated solutions on 11

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -21,48 +21,77 @@ function flatten(arr) {
     // if first element is an array, recursively flatten it
     if(Array.isArray(arr[0])) {
                                               // slices everything off of the first index
-        return flatten(arr[0]).concat(flatten(arr.slice(1)));
+        // return flatten(arr[0]).concat(flatten(arr.slice(1)));
+        return [...flatten(arr[0]) , ...flatten(arr.slice(1))];
+
     }
     // if element is not an array, put it in array and concat with flatten remainder of array
-    return [arr[0]].concat(flatten(arr.slice(1)));
+    // return [arr[0]].concat(flatten(arr.slice(1)));
+    return [arr[0] , ...flatten(arr.slice(1))];
 }
 
 
-// console.log(flatten([])); // []
-// console.log(flatten([1, 2])); // [1, 2]
-// console.log(flatten([1, 2, 3, 4, 5])); // [1, 2, 3, 4, 5]
+console.log(flatten([])); // []
+console.log(flatten([1, 2])); // [1, 2]
+console.log(flatten([1, 2, 3, 4, 5])); // [1, 2, 3, 4, 5]
 console.log(flatten([1,
                       [2,
                         [3]]])); // [1, 2, 3]
 
 
+// teacher solution
+function flatten2(arr) {
+  //!!START
+  let newArray = [];
 
-// first try not working
-// let newArr = [];
+  arr.forEach(function (contents) {
+      if (Array.isArray(contents)) {
+        newArray.push(...flatten2(contents));
+      } else {
+        newArray.push(contents);
+      }
+  });
 
-// function flatten2(arr) {
-//     if(arr.length === 0) {
-//         return arr;
-//     } else {
-//         console.log(arr);
-//         let last = arr[arr.length-1];
-//         console.log(last);
+  return newArray;
+  //!!END
+}
 
-//         arr.pop(last);
-//         console.log(arr);
-//         newArr.push(last);
-//         console.log(newArr);
+console.log(flatten2([])); // []
+console.log(flatten2([1, 2])); // [1, 2]
+console.log(flatten2([1, 2, 3, 4, 5])); // [1, 2, 3, 4, 5]
+console.log(flatten2([1,
+                      [2,
+                        [3]]])); // [1, 2, 3]
 
-//         return flatten2(arr);
-//     }
 
-// }
 
-// console.log(flatten2([])); // []
-// console.log(flatten2([1, 2])); // [1, 2]
-// console.log(flatten2([1,
-//                       [2,
-//                         [3]]])); //
+
+// first try not working, not dealing with nested arrays
+let newArr = [];
+
+function flatten3(arr) {
+    if(arr.length === 0) {
+        return newArr;
+    } else {
+        console.log(arr);
+        // let last = arr[arr.length-1];
+
+        let last = arr.pop();
+        console.log(last);
+        console.log(arr);
+        newArr.unshift(last);
+        console.log(newArr);
+
+        return flatten3(arr);
+    }
+
+}
+
+console.log(flatten3([])); // []
+console.log(flatten3([1, 2])); // [1, 2]
+console.log(flatten3([1,
+                      [2,
+                        [3]]])); //
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
